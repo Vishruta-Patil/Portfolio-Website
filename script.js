31fbdd5c7213565d592b88d4bb00a5cfb9c4dd63
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-08-26 12:17:36 +0530

    Minor: Removed unnecessary folders

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ tabsContainer.addEventListener("click", (e)=>{
         tabsContainer.querySelector('.active').classList.remove("active");
         e.target.classList.add("active");
         const target = e.target.getAttribute("data-target");
-        // console.log(target);
         aboutSection.querySelector(".tab-content.active").classList.remove("active");
         aboutSection.querySelector(target).classList.add("active");
     }
