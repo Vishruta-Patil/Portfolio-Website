92aa7aecda7e339152b95a03de4a6e551498d8c6
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-08-26 12:24:57 +0530

    Minor: modified setTimeout

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener("load",()=>{
     document.querySelector(".page-loader").classList.add("fade-out");
     setTimeout(()=>{
         document.querySelector(".page-loader").style.display='none';
-    },600);
+    },300);
 });
 
 
